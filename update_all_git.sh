e95a82a620aeca8b295e18326f4357a573dc39b4
Андрей Лученок <zakaz.belon@gmail.com>
2022-08-03 22:00:19 +0300

    update_script

--- update_all_git.sh
+++ update_all_git.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # This script update remote repository^ github, gitlab, bitbucket
-Green='\033[0;32m'
+comment=$1
+git add --all
+git commit -m "$comment"
+Green="\033[0;32m"
 git push origin_github --all
 echo "${green}Github updated!"
-#git push origin_gitlab --all
-#echo "Gitlab updated!"
-#git push origin_bitbucket --all
-#echo "bitbucket updated!"
+git push origin_gitlab --all
+echo "Gitlab updated!"
+git push origin_bitbucket --all
+echo "bitbucket updated!"
