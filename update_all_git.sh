33b40b1a69465caaab7d630c9f5bbaffe815105b
Андрей Лученок <zakaz.belon@gmail.com>
2022-08-03 22:31:58 +0300

    test_color_echo4

--- update_all_git.sh
+++ update_all_git.sh
@@ -5,8 +5,8 @@ git add --all
 git commit -m "$comment"
 Green=\033[32m
 git push origin_github --all
-echo -e "\033[32mGithub updated\033[32m"
+echo -e "\033[32mGithub updated!\033[m"
 git push origin_gitlab --all
-echo "Gitlab updated!${green}"
+echo -e "\033[32mGitlab updated!\033[m"
 git push origin_bitbucket --all
-echo "bitbucket updated!${green}"
+echo -e "\033[32mbitbucket updated!\033[m"
